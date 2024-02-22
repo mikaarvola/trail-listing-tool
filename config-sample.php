47d9ef4456d5afba0bec0f60f4587c3602195ffd
Mika Arvola <mika@gwitted.com>
2024-02-22 14:56:16 +0200

    Added base-url for images

--- config-sample.php
+++ config-sample.php
@@ -4,5 +4,10 @@
 $code = 'DEADBEEFDEADCAFEBEBEBEBE==';
 
 // Insert department code below, for example 1234567890
+// Deprecated, will be removed soon since API has now built-in functions to limit access
 $department1 = 'department_here';
+
+// Insert baseurl for images below, remember to define protocol and ending slash. Trail public API doesn't publish this infomation,
+// that's why we need to put images manually in folder. Use "M1234567.jpg" as filenaming.
+$img_baseurl = 'https://example.tld//path/to/images/';
 ?>
